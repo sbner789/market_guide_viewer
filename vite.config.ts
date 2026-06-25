b936c1d498c8e7b0965d4f7545366f2956d68c76
rickchoi <sbner182@gmail.com>
2026-06-25 18:01:25 +0900

    feat: add vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: true, // 또는 '0.0.0.0'
+    port: 5173
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
